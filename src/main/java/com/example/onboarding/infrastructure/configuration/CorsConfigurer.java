a4a84a2e2069282486fe24d4d56cbd154610e6d1
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-11-08 22:47:53 +0900

    hotfix: change server host

--- src/main/java/com/example/onboarding/infrastructure/configuration/CorsConfigurer.java
+++ src/main/java/com/example/onboarding/infrastructure/configuration/CorsConfigurer.java
@@ -1,6 +1,5 @@
 package com.example.onboarding.infrastructure.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,11 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfigurer implements WebMvcConfigurer {
 
-    @Value("${server.host}")
-    private String host;
-
     private final String[] ORIGIN_WHITE_LIST = {
-            "http://localhost:8080"
+            "http://localhost:8080",
     };
 
     /**
